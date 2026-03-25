b186fd3035129df860c42488689d34c234456a56
Rondi650 <rondinellenunes123@gmail.com>
2026-03-24 21:35:49 -0300

    configuracao da area administativa relacionada aos models (banco de dados)

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact.models import Contact
 
 # Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id','first_name', 'last_name', 'phone', 'email',
+    ordering = '-id', # o - na frente deixa em ordem decrescente
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 20
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name'
+    list_display_links = 'id', 'phone'
--- contact/models.py
+++ contact/models.py
@@ -11,4 +11,5 @@ class Contact(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
 
-
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
