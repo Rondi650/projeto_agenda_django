1f4bb0ca3ba3acb00f536f4af2f0fda56f7b2afd
Rondi650 <rondinellenunes123@gmail.com>
2026-03-24 21:06:58 -0300

    aula dia 24/06/2026, configurado models no django

--- contact/views.py
+++ contact/views.py
@@ -4,5 +4,10 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context= {
+            "title": 'index',
+            "exemplo": "Esse e um texto de exemplo para a pagina",
+            "outro_exemplo": 'Outro texto de exemplo'
+        }
     )
 
