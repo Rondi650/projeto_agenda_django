d70e45436182943fd2f087ab672d033be9ea8219
Rondi650 <rondinellenunes123@gmail.com>
2026-03-27 10:57:41 -0300

    melhoria do search da pagina usando "Q" do django para uso de "OR" insercao de if dentro do index para deixar dinamico quando nao encontrar dados digitados com a mensagem "Nenhum contato encontrado"

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.models import Contact
 from django.http import Http404
+from django.core.handlers.wsgi import WSGIRequest
+from django.db.models import Q
+from rich import print
 
 
-def index(request):
+def index(request: WSGIRequest):
     contacts = Contact.objects.all() \
         .filter(show=True) \
         .order_by('-id')[:10]
@@ -19,8 +22,35 @@ def index(request):
         context
     )
 
+def search(request: WSGIRequest):
+    search_value = request.GET.get('q', '').strip()
+    
+    if search_value == '':
+        return redirect('contact:index')
 
-def contact(request, contact_id):
+    contacts = Contact.objects.all() \
+        .filter(show=True) \
+        .filter(Q(first_name__icontains=search_value) | 
+                Q(phone__icontains=search_value) | 
+                Q(email__icontains=search_value) | 
+                Q(last_name__icontains=search_value)) \
+        .order_by('-id')
+    
+    print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+        'title': 'Search'
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def contact(request: WSGIRequest, contact_id: int):
     single_contact = Contact.objects.filter(
         id=contact_id,
         show=True
