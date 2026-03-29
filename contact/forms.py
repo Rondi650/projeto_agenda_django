985e468e1a53df29db377e4c017d99a7e7d3ac47
Rondi650 <rondinellenunes123@gmail.com>
2026-03-29 12:10:45 -0300

    inclusao de add_error em forms.py para validacao de entrada no forms

--- contact/forms.py
+++ contact/forms.py
@@ -1,30 +1,60 @@
+from typing import Any, Mapping
+
 from django.core.exceptions import ValidationError
 from django import forms
+
 
 from contact.models import Contact
 
 class ContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        print('Print do attr: ', self.fields['first_name'].widget.attrs)
+
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Novo placeholder'
+            }
+        ),
+        label='Seu primeiro nome',
+        help_text='Texto de ajuda para seu usuario'
+    )
+
     class Meta:
         model = Contact
         fields = 'first_name', 'last_name', 'phone'
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={'placeholder': 'Seu nome'})
+        # }
 
-    
+
     def clean(self):
         cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
         print(cleaned_data)
-        self.add_error(
-            
-            'first_name',
-            ValidationError(
-                'Mensagem de erro',
-                code='invalid'
-            )
-        )
-        self.add_error(
-            'last_name',
-            ValidationError(
-                'Mensagem de erro 2',
-                code='invalid'
-            )
-        )
+
+        if first_name == last_name:
+            msg = ValidationError(
+                    'Os nomes nao podem ser iguais',
+                    code='invalid')
+            self.add_error('first_name',msg)
+            self.add_error('last_name',msg)
+
         return super().clean()
+    
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if first_name == 'abc':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Entrada invalida',
+                    code='invalid'
+                )
+            )
+        return first_name
