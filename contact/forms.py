dbf60aad7153a0960401026f964fa5cba8dd506c
Rondi650 <rondinellenunes123@gmail.com>
2026-03-29 12:18:37 -0300

    inserido todos os campos para forms atualizando a classe Meta em ContactForms no arquivo forms.py

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,5 @@
 from typing import Any, Mapping
+from unicodedata import category
 
 from django.core.exceptions import ValidationError
 from django import forms
@@ -6,30 +7,36 @@ from django import forms
 
 from contact.models import Contact
 
+
 class ContactForm(forms.ModelForm):
+    # ------ CRIACAO --------
+    # modo 1
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         print('Print do attr: ', self.fields['first_name'].widget.attrs)
 
+    # modo 2
     first_name = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'placeholder': 'Novo placeholder'
+                # 'placeholder': 'Novo placeholder'
             }
         ),
-        label='Seu primeiro nome',
-        help_text='Texto de ajuda para seu usuario'
+        # label='Seu primeiro nome',
+        # help_text='Texto de ajuda para seu usuario'
     )
 
+    # modo 3
     class Meta:
         model = Contact
-        fields = 'first_name', 'last_name', 'phone'
+        fields = 'first_name', 'last_name', 'phone', \
+                 'email', 'description', 'category'
         # widgets = {
         #     'first_name': forms.TextInput(attrs={'placeholder': 'Seu nome'})
         # }
 
-
+    # ------ ERROS --------
     def clean(self):
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
@@ -38,13 +45,12 @@ class ContactForm(forms.ModelForm):
 
         if first_name == last_name:
             msg = ValidationError(
-                    'Os nomes nao podem ser iguais',
-                    code='invalid')
-            self.add_error('first_name',msg)
-            self.add_error('last_name',msg)
+                'Os nomes nao podem ser iguais',
+                code='invalid')
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
-    
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
