224a10eb01a1b5326dbc0dd4040a14a374d3157c
Rondi650 <rondinellenunes123@gmail.com>
2026-03-31 22:09:15 -0300

    tela de login e logout, usando padroes do django para users

--- contact/forms.py
+++ contact/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from contact.models import Contact
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.contrib import messages
+
 
 
 class ContactForm(forms.ModelForm):
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from contact.forms import RegisterForm
+from django.contrib import auth
 
 
 def register(request):
     form = RegisterForm()
+
+    messages.info(request, 'Boa noite')
 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
 
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuarior registrado')
+            return redirect('contact:login')
+            
 
     context = {
         'form': form
@@ -21,3 +28,30 @@ def register(request):
         'contact/register.html',
         context
     )
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso')
+            return redirect('contact:index')
+        messages.error(request, 'Login invalido')
+
+    context = {
+        'form': form
+    }
+
+    return render(
+        request,
+        'contact/login.html',
+        context
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
