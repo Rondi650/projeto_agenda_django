0774fafdcfd63282bbe66f7dc62c5e6c46b4e39e
Rondi650 <rondinellenunes123@gmail.com>
2026-04-01 15:50:43 -0300

    preparativos para fazer deploy

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,10 +1,12 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 
 from contact.models import Contact
 
 
+@login_required(login_url='contact:login')
 def index(request):
     contacts = Contact.objects \
         .filter(show=True)\
@@ -26,6 +28,7 @@ def index(request):
     )
 
 
+@login_required(login_url='contact:login')
 def search(request):
     search_value = request.GET.get('q', '').strip()
 
@@ -59,6 +62,7 @@ def search(request):
     )
 
 
+@login_required(login_url='contact:login')
 def contact(request, contact_id):
     # single_contact = Contact.objects.filter(pk=contact_id).first()
     single_contact = get_object_or_404(
